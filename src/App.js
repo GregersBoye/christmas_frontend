aefa1c9e3e51c1628798a04e96a08a18ba62329b
Gregers Boye-Jacobsen <gbj@cego.dk>
2020-11-22 00:49:44 +0100

    install mini.css and downgraded typescritp
    typescript had to be downgraded due to a bug introduced in 4.1.x
    Installed mini.css for css-frameworkk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import 'mini.css';
 
-function App() {
+const App = () => {
+  const [pinStates, setPinStates] = useState([]);
+
+  useEffect(() => {
+    fetch('http://192.168.1.22:1224/state')
+      .then(response => response.json())
+      .then(data => setPinStates(data));
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <h1>Christmas-light controller</h1>
       </header>
+
+      <section>
+
+      </section>
+
     </div>
   );
 }
